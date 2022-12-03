2defff7ae9db72cf6e760babb1c563952b6e0d5c
datajake1999 <datajake@gmail.com>
2022-12-03 11:46:59 -0800

    Reindent code.

--- silence.c
+++ silence.c
@@ -7,56 +7,56 @@
 // Helper function to fill a buffer with silence
 void FillBufferWithSilence(LPSTR buffer, DWORD bufferSize)
 {
-    for (DWORD i = 0; i < bufferSize; i++)
-    {
-        buffer[i] = 0x80;
-    }
+	for (DWORD i = 0; i < bufferSize; i++)
+	{
+		buffer[i] = 0x80;
+	}
 }
 
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCommandLine, int cmdShow)
 {
-    // Set up wave format structure
-    WAVEFORMATEX waveFormat;
-    waveFormat.wFormatTag = WAVE_FORMAT_PCM;
-    waveFormat.nChannels = 1;
-    waveFormat.nSamplesPerSec = samplerate;
-    waveFormat.nAvgBytesPerSec = samplerate;
-    waveFormat.nBlockAlign = 1;
-    waveFormat.wBitsPerSample = 8;
-    waveFormat.cbSize = 0;
+	// Set up wave format structure
+	WAVEFORMATEX waveFormat;
+	waveFormat.wFormatTag = WAVE_FORMAT_PCM;
+	waveFormat.nChannels = 1;
+	waveFormat.nSamplesPerSec = samplerate;
+	waveFormat.nAvgBytesPerSec = samplerate;
+	waveFormat.nBlockAlign = 1;
+	waveFormat.wBitsPerSample = 8;
+	waveFormat.cbSize = 0;
 
-    // Set up wave header structure
-    WAVEHDR waveHeader;
-    waveHeader.lpData = (char *)malloc(samplerate);
-    waveHeader.dwBufferLength = samplerate;
-    waveHeader.dwBytesRecorded = 0;
-    waveHeader.dwUser = 0;
-    waveHeader.dwFlags = 0;
-    waveHeader.dwLoops = 0;
-    waveHeader.lpNext = 0;
-    waveHeader.reserved = 0;
+	// Set up wave header structure
+	WAVEHDR waveHeader;
+	waveHeader.lpData = (char *)malloc(samplerate);
+	waveHeader.dwBufferLength = samplerate;
+	waveHeader.dwBytesRecorded = 0;
+	waveHeader.dwUser = 0;
+	waveHeader.dwFlags = 0;
+	waveHeader.dwLoops = 0;
+	waveHeader.lpNext = 0;
+	waveHeader.reserved = 0;
 
-    // Fill the buffer with silence
-    FillBufferWithSilence(waveHeader.lpData, waveHeader.dwBufferLength);
+	// Fill the buffer with silence
+	FillBufferWithSilence(waveHeader.lpData, waveHeader.dwBufferLength);
 
-    // Open the default waveform-audio output device
-    HWAVEOUT hWaveOut = NULL;
-    waveOutOpen(&hWaveOut, WAVE_MAPPER, &waveFormat, 0, 0, 0);
+	// Open the default waveform-audio output device
+	HWAVEOUT hWaveOut = NULL;
+	waveOutOpen(&hWaveOut, WAVE_MAPPER, &waveFormat, 0, 0, 0);
 
-    while(1)
-    {
-        // Prepare and write the wave header
-        waveOutPrepareHeader(hWaveOut, &waveHeader, sizeof(WAVEHDR));
-        waveOutWrite(hWaveOut, &waveHeader, sizeof(WAVEHDR));
+	while(1)
+	{
+		// Prepare and write the wave header
+		waveOutPrepareHeader(hWaveOut, &waveHeader, sizeof(WAVEHDR));
+		waveOutWrite(hWaveOut, &waveHeader, sizeof(WAVEHDR));
 
-        // Wait for the sound to finish playing
-        Sleep(1000);
-    }
+		// Wait for the sound to finish playing
+		Sleep(1000);
+	}
 
-    // Clean up
-    waveOutUnprepareHeader(hWaveOut, &waveHeader, sizeof(WAVEHDR));
-    waveOutClose(hWaveOut);
-    free(waveHeader.lpData);
+	// Clean up
+	waveOutUnprepareHeader(hWaveOut, &waveHeader, sizeof(WAVEHDR));
+	waveOutClose(hWaveOut);
+	free(waveHeader.lpData);
 
-    return 0;
+	return 0;
 }
